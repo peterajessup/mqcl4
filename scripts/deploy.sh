343ec193190c0cff1b49a9b6fe3a3646786dad5d
peterajessup <peterajessup@users.noreply.github.com>
2021-08-13 16:53:33 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,16 +3,16 @@ oc project ibmmq
 set +e
 # Remove the runnning queue manager instance (if any)
 
-oc delete QueueManager mqcl1
+oc delete QueueManager mqcl3
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mqcl1route
-oc delete secret mqcl1key
-oc delete configMap cl1-mqsc
+oc delete route mqcl3route
+oc delete secret mqcl3key
+oc delete configMap cl3-mqsc
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mqcl1Route.yaml
-oc create secret tls mqcl1key --cert=./tls/tls.crt --key=./tls/tls.key
+oc apply -f mqcl3Route.yaml
+oc create secret tls mqcl3key --cert=./tls/tls.crt --key=./tls/tls.key
 oc create -f mqsc/mqsc.yaml
 
 set -e
