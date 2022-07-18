6a15563ae7bf23f31793a1cb91cd04a751e56470
peterajessup <peterajessup@users.noreply.github.com>
2022-07-18 16:09:26 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,16 +3,16 @@ oc project mq
 set +e
 # Remove the runnning queue manager instance (if any)
 
-oc delete QueueManager mqcl3
+oc delete QueueManager mqcl4
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mqcl3route
-oc delete secret mqcl3key
-oc delete configMap cl3-mqsc
+oc delete route mqcl4route
+oc delete secret mqcl4key
+oc delete configMap cl4-mqsc
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mqcl3Route.yaml
-oc create secret tls mqcl3key --cert=./tls/tls.crt --key=./tls/tls.key
+oc apply -f mqcl4Route.yaml
+oc create secret tls mqcl4key --cert=./tls/tls.crt --key=./tls/tls.key
 oc create -f mqsc/mqsc.yaml
 
 set -e
